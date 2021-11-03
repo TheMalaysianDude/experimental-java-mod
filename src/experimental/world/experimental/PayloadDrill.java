52cf474eda24df1a851ec7da2cbd516136ea1b58
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2021-11-03 14:14:53 +0800

    changes

--- src/experimental/world/experimental/PayloadDrill.java
+++ src/experimental/world/experimental/PayloadDrill.java
@@ -31,6 +31,8 @@ public class PayloadDrill extends PayloadBlock {
 		rotate = true;
 		outputsPayload = true;
 		
+		consumes.add(new ConsumeItemDynamic((PayloadDrillBuild e) -> e.recipe != null ? e.recipe().requirements : ItemStack.empty));
+		
 		config(Block.class, (PayloadDrillBuild tile, Block block) -> {
 			if(tile.recipe != block) tile.progress = 0f;
 			if(canProduce(block)){
@@ -47,14 +49,20 @@ public class PayloadDrill extends PayloadBlock {
 		public @Nullable Block recipe;
 		public float progress;
 		
-		
-		public @Nullable Block recipe(){
-			return recipe;
-		}
-		
         @Override
         public void buildConfiguration(Table table){
             ItemSelection.buildTable(table, content.blocks().select(PayloadDrill.this::canProduce), () -> recipe, this::configure);
+        }
+		
+		@Override
+        public boolean onConfigureTileTapped(Building other){
+            if(this == other){
+                deselect();
+                configure(null);
+                return false;
+            }
+
+            return true;
         }
 		
         @Override
@@ -63,15 +71,75 @@ public class PayloadDrill extends PayloadBlock {
         }
 		
 		@Override
+        public void updateTile(){
+            super.updateTile();
+            boolean produce = recipe != null && consValid() && payload == null;
+
+            if(produce){
+                progress += buildSpeed * edelta();
+
+                if(progress >= recipe.buildCost){
+                    consume();
+                    payload = new BuildPayload(recipe, team);
+                    Fx.placeBlock.at(x, y, payload.size() / tilesize);
+                    payVector.setZero();
+                    progress %= 1f;
+                }
+            }
+
+            heat = Mathf.lerpDelta(heat, Mathf.num(produce), 0.15f);
+            time += heat * delta();
+
+            moveOutPayload();
+        }
+
+        @Override
+        public void draw(){
+            Draw.rect(region, x, y);
+            Draw.rect(outRegion, x, y, rotdeg());
+			
+            if(recipe != null){
+                Drawf.shadow(x, y, recipe.size * tilesize * 2f, progress / recipe.buildCost);
+                Draw.draw(Layer.blockBuilding, () -> {
+                    Draw.color(Pal.accent);
+
+                    for(TextureRegion region : recipe.getGeneratedIcons()){
+                        Shaders.blockbuild.region = region;
+                        Shaders.blockbuild.time = time;
+                        Shaders.blockbuild.progress = progress / recipe.buildCost;
+
+                        Draw.rect(region, x, y, recipe.rotate ? rotdeg() : 0);
+                        Draw.flush();
+                    }
+
+                    Draw.color();
+                });
+                Draw.z(Layer.blockBuilding + 1);
+                Draw.color(Pal.accent, heat);
+
+                Lines.lineAngleCenter(x + Mathf.sin(time, 10f, Vars.tilesize / 2f * recipe.size + 1f), y, 90, recipe.size * Vars.tilesize + 1f);
+
+                Draw.reset();
+            }
+
+            drawPayload();
+
+            Draw.z(Layer.blockBuilding + 1.1f);
+            Draw.rect(topRegion, x, y);
+        }
+		
+		@Override
         public void write(Writes write){
             super.write(write);
             write.s(recipe == null ? -1 : recipe.id);
+			write.f(progress);
         }
 
         @Override
         public void read(Reads read, byte revision){
             super.read(read, revision);
             recipe = Vars.content.block(read.s());
+			progress = read.f();
         }
 	}
 }
