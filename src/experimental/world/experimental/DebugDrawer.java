c4819ada859fbaae597a9aa00c47e90a122b0218
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2022-02-06 09:40:49 +0800

    added DebugDrawer

---
+++ src/experimental/world/experimental/DebugDrawer.java
@@ -0,0 +1,17 @@
+package experimental.world.experimental;
+
+import arc.struct.*;
+import mindustry.world.*;
+import mindustry.gen.*;
+
+public class DebugDrawer extends Block{
+	Seq<DrawBlock> drawers = new Seq<>();
+	
+	public DebugDrawer(String name){
+		super(name);
+	}
+	
+	public DebugDrawerBuild extends Building{
+		
+	}
+}
