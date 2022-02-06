d5316991b0355ed3c3c19bf1bccf6e4043fe7efa
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2022-02-06 09:41:21 +0800

    added DrawLiquidScale via draw Folder

---
+++ src/experimental/world/DrawLiquidScale.java
@@ -0,0 +1,40 @@
+package experimental.world.draw;
+
+import arc.*;
+import arc.graphics.g2d*;
+import mindustry.graphics.*;
+import mindustry.world.*;
+import mindustry.world.draw.*;
+import mindustry.gen.*;
+import experimental.world.experimental.*;
+
+public class DrawLiquidScale extends DrawBlock{
+	public TextureRegion liquid, top;
+	
+	@Override
+    public void draw(Building build){
+		Draw.rect(build.block.region, build.x, build.y);
+		
+		Draw.color(build.liquids.current().color);
+		Draw.rect(liquid, build.x, build.y, 
+			(liquid.width/4) * totalLiquids,
+			(liquid.height/4) * totalLiquids
+		);
+		Draw.color();
+		
+		if(top.found()) Draw.rect(top, build.x, build.y);
+    }
+
+    @Override
+    public void load(Block block){
+        liquid = Core.atlas.find(block.name + "-liquid");
+        top = Core.atlas.find(block.name + "-top");
+		
+		DebugDrawer.drawers.add(this);
+    }
+
+    @Override
+    public TextureRegion[] icons(Block block){
+        return top.found() ? new TextureRegion[]{block.region, top} : new TextureRegion[]{block.region};
+    }
+}
