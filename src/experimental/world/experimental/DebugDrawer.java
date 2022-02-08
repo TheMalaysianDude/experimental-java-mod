b7931d47d9b285c14eaf3a0c6fcd0741897e9b8e
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2022-02-08 21:27:10 +0800

    Update DebugDrawer.java

--- src/experimental/world/experimental/DebugDrawer.java
+++ src/experimental/world/experimental/DebugDrawer.java
@@ -1,21 +1,64 @@
 package experimental.world.experimental;
 
-import arc.struct.*;
-import mindustry.world.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.scene.ui.layout.*;
+import arc.util.*;
+import arc.util.io.*;
+import mindustry.*;
 import mindustry.gen.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
 import mindustry.world.draw.*;
+
 import experimental.world.draw.*;
 
 public class DebugDrawer extends Block{
 	public static DrawBlock[] drawers = {
-		DrawLiquidScale = new DrawLiquidScale
+		new DrawLiquidScale
 	};
 	
 	public DebugDrawer(String name){
 		super(name);
+		configurable = true;
+		update = true;
+		drawDisabled = false;
+		autoResetEnabled = false;
+		group = BlockGroup.none
+		
+		config(float.class, (DebugDrawerBuild tile, float value) -> tile.progress = value);
+		drawer(DrawBlock.class, (DebugDrawerBuild tile, DrawBlock value) -> tile.drawer = value);
 	}
 	
 	public class DebugDrawerBuild extends Building{
+		public @Nullable DrawBlock drawer;
+		public Float progress;
 		
+		public DrawBlock drawer(){
+			return drawer;
+		}
+		
+		public float progress(){
+			return progress;
+		}
+		
+		@Override
+		public void draw(){
+			drawer.draw(this);
+		}
+		
+		@Override
+		public void buildConfiguration(Table table){
+			table.slider(0f, 1f, 0.01f, progress, value -> {
+				configure(value);
+			}).growX().row();
+			table.pane(it -> {
+				for (DrawBlock drawBlock : drawers){
+					it.button(drawBlock.getClass().getSimpleName(), () -> {
+						configure(drawBlock);
+					}).growX().row();
+				}
+			});
+		}
 	}
 }
