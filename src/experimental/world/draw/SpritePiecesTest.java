d36cebc6fbf9b10e1420d3462337d7cdadd9eda4
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2022-02-10 22:41:59 +0800

    added SpritePiecesTest Draw

---
+++ src/experimental/world/draw/SpritePiecesTest.java
@@ -0,0 +1,39 @@
+package experimental.world.draw;
+
+import arc.*;
+import arc.graphics.g2d.*;
+import mindustry.graphics.*;
+import mindustry.world.*;
+import mindustry.gen.*;
+
+import experimental.world.experimental.DebugDrawer.*;
+
+public class SpritePiecesTest extends ExDrawBlock{
+	public TextureRegion sprite;
+	public int split = 2;
+	//public float[] positions = {};
+	
+	@Override
+	public void draw(DebugDrawerBuild build){
+		Block type = (Block)build.block;
+		
+		int width = sprite.width;
+		int height = sprite.height;
+		
+		for(int i = 0; i < size; i++){
+			Draw.rect(sprite, x - (width/2) + (i*width/split), y - (height/2) + (i*height/split)
+			
+			);
+		}
+	}
+	
+	@Override
+	public void load(Block block){
+		sprite = Core.atlas.find(block.name + "-sprite");
+	}
+	
+	@Override
+	public TextureRegion[] icons(Block block){
+		return new TextureRegion[]{block.region, sprite};
+	}
+}
