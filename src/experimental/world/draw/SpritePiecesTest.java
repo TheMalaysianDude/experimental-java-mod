85320863c60930761aa6f96d9641190cf05bbc50
TheMalaysianDude <57954772+TheMalaysianDude@users.noreply.github.com>
2022-02-11 02:17:20 +0800

    Update SpritePiecesTest.java

--- src/experimental/world/draw/SpritePiecesTest.java
+++ src/experimental/world/draw/SpritePiecesTest.java
@@ -9,8 +9,9 @@ import mindustry.gen.*;
 import experimental.world.experimental.DebugDrawer.*;
 
 public class SpritePiecesTest extends ExDrawBlock{
-	public TextureRegion sprite;
 	public int split = 2;
+	public TextureRegion sprite;
+	public TextureRegion[split*split] pieces;
 	//public float[,] positions = {};
 	
 	@Override
@@ -26,12 +27,19 @@ public class SpritePiecesTest extends ExDrawBlock{
 		Draw.rect(type.region, build.x, build.y);
 		for(int x = 0; x < split; x++){
 			for(int y = 0; y < split; y++){
+				TextureRegion piece = pieces[split * y + x];
+				Draw.rect(piece,
+					build.x + build.x - splitWidth/2 + splitWidth*x, build.y - splitHeight/2 + splitHeight*y,
+					0
+				);
+				/*
 				Draw.rect(sprite,
 					build.x - splitWidth/2 + splitWidth*x, build.y - splitHeight/2 + splitHeight*y,
 					splitWidth, splitHeight,
 					splitWidth*x, splitHeight*y,
 					0
 				);
+				*/
 			}
 		}
 	}
@@ -39,6 +47,15 @@ public class SpritePiecesTest extends ExDrawBlock{
 	@Override
 	public void load(Block block){
 		sprite = Core.atlas.find(block.name + "-sprite");
+		
+		for(int x = 0; x < split; x++){
+			for(int y = 0; y < split; y++){
+				pieces[split * y + x] = new TextureRegion(sprite, 
+					sprite.u/2 * x, sprite.v/2 * y, 
+					sprite.u2/2 * (x + 1), sprite.v2 * (y + 1)
+				);
+			}
+		}
 	}
 	
 	@Override
