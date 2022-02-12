97752fa61fe1a75a9060a4a47c69bf435fc6a755
TheMalaysianDude <goddangito2@gmail.com>
2022-02-12 08:45:49 +0800

    Update SpritePiecesTest.java

--- src/experimental/world/draw/SpritePiecesTest.java
+++ src/experimental/world/draw/SpritePiecesTest.java
@@ -1,6 +1,7 @@
 package experimental.world.draw;
 
 import arc.*;
+import arc.math.*;
 import arc.util.*;
 import arc.graphics.g2d.*;
 import mindustry.graphics.*;
@@ -24,14 +25,35 @@ public class SpritePiecesTest extends ExDrawBlock{
 		Draw.rect(type.region, build.x, build.y);
 		for(int x = 0; x < split; x++){
 			for(int y = 0; y < split; y++){
-				var magnitude = build.block.size * 8 * build.progress / split;
+				//var magnitude = build.block.size * 8 * build.progress / split;
+				float index = split * y + x;
+				
+				/*
+				split = 2
+				process = {
+				 -0    - 0.25
+				 -0.25 - 0.5
+				 -0.5  - 0.75
+				 -0.75 - 1
+				}
+				
+				progress = 0 - 1
+				
+				Mathf.clamp(build.progress, (1/(float)(split*split))*index, (1/(float)(split*split))*(index+1)
+				*/
+				
+				//basically 0 to 1 for each piece
+				//float process = (index+1)/(float)(split*split);
+				float progress = Mathf.clamp(build.progress, (1/(float)(split*split))*index, (1/(float)(split*split))*(index+1)) /  (1/(float)(split*split))*(index+1);
 				var powerX = x - (split - 1) / 2f;
 				var powerY = (split - 1) / 2f - y;
-
+				
 				TextureRegion piece = pieces[x][y];
 				Draw.rect(piece, 
-					build.x + magnitude * powerX + piece.width * powerX / 4,
-					build.y + magnitude * powerY + piece.height * powerY / 4
+					build.x + powerX + piece.width * powerX / 4,
+					build.y + powerY + piece.height * powerY / 4,
+					piece.width/4 * progress,
+					piece.height/4 * progress
 				);
 			}
 		}
