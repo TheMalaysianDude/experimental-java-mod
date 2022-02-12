ad4eddba734861b99b5716fd6dae6b115963d821
TheMalaysianDude <goddangito2@gmail.com>
2022-02-12 11:02:02 +0800

    Update SpritePieces.java

--- src/experimental/world/draw/SpritePiecesTest.java
+++ src/experimental/world/draw/SpritePiecesTest.java
@@ -46,8 +46,9 @@ public class SpritePiecesTest extends ExDrawBlock{
 				//float process = (index+1)/(float)(split*split);
 				//distance between progress and index/split
 				//plus 1 minus 1 to prevent division by 0
-				float progress = Mathf.clamp(build.progress, 1f/(split*split)*index, 1);
-				Log.info(1f/(split*split)*index);
+				float percentage = Mathf.clamp(build.progress, 1f/(split*split)*index, 1f/(split*split)*(index+1));
+				float progress = percentage;
+				Log.info(percentage);
 				var powerX = x - (split - 1) / 2f;
 				var powerY = (split - 1) / 2f - y;
 				
